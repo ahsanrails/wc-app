111e3ea3bd7e72937fc426314009ca7c7165b7ea
ahsanrails <sunnyahsan500@gmail.com>
2016-03-22 18:26:38 +0500

    complete-login-signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,16 +4,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-# --
-def after_sign_in_path_for(resource)
-  request.env['omniauth.origin'] || stored_location_for(resource) || root_path
-end
 
-def after_sign_out_path_for(resource)
-  new_user_session_path()
-end 
-
-# --
 
   # before_action :authenticate_user!
 
